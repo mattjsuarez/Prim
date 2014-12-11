c0e9ee1985dbea54038b6a6b2a04c1e105607fe1
Matt Suarez <mjsuarez@wpi.edu>
2014-12-11 17:11:54 -0500

    router functions to retrieve doctors and patients from db
    retrieve both doctors and patients from db using their names or ids

--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,53 @@ var router = express.Router();
 var Client = require('mariasql');
 var inspect = require('util').inspect;
 var c = new Client();
+
+//connect to the database
+c.connect({
+    host: 'localhost',
+    user: 'mjsuarez',
+    password: 'mjsuarez_pw',
+    db: 'mjsuarez_db'
+});
+
+//connect to index.jade file to render it
+router.get('/', function(req, res) {
+  res.render('../views/index');
+});
+
+//retrieve a doctor's information by the doctor's name
+router.get('retrieveDoctorByName/:name', function(req, res) {
+	var name = req.param("name"); //retrieve the value from the parameter name
+	var doctorInfo = c.query("SELECT * FROM Doctors WHERE Name= :nam", {nam:name}); //generate a query. asks database for a doctor given a name
+	doctorInfo.on('result', function(res) {
+		res.on('row',function(row) {
+			var obj = JSON.stringify(inspect(row));
+			res.send(obj); //...and return the doctor information
+		});
+	});
+});
+
+//retrieve a patient's information by the patient's id number
+router.get('retrievePatientByID/:id', function(req, res) {
+	var id = req.param("id");
+	var patientInfo = c.query("SELECT * FROM Patients WHERE Id= :id", {id:id}); //generate a query asks database for a patient by patient's id
+	patientInfo.on('result', function(res) {
+		res.on('row', function(row) {
+			var obj = JSON.stringify(inspect(row));
+			res.send(obj);
+		});
+	});
+});
+
+//retrieve a patient's information by the patient's name
+router.get('retrievePatientByID/:name', function(req, res) {
+	var name = req.param("name");
+	var patientInfo = c.query("SELECT * FROM Patients WHERE Name= :nam", {nam:name}); //generate a query asks database for a patient by patient's id
+	patientInfo.on('result', function(res) {
+		res.on('row', function(row) {
+			var obj = JSON.stringify(inspect(row));
+			res.send(obj);
+		});
+	});
+});
+
