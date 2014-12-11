85b68ecb4eb06170d107e3aef8c759747c0f0269
ivanempire <ivanempire@gmail.com>
2014-12-11 17:52:38 -0500

    Initial populate script

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 //Main function when body loads
 $(document).ready(function() {
-	
+	panelExpand();
 });
+
+var panelExpand = function() {
+	$("#expandInfo").click(function() {
+		var userID = $("#userID").text();
+		var request = $.ajax({
+        	url:'/retrieveGraphData/'+userID,
+        	type:'GET',
+        	success:function(msg) {
+    			console.log(msg);	    
+        	}
+    	});
+    	request.fail(function(jqXHR, textStatus) {
+        	console.log("Request failed: " + textStatus);
+    	});
+	});
+};
